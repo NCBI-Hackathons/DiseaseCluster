fe0e3a222e2961190ee5106c4e502e14a66d1807
Ubuntu <ubuntu@ip-172-16-240-82.ec2.internal>
2018-11-12 14:42:53 +0000

    Updated seaborn/matplotlib stuff. Adding Hackathon viz for Matt

---
+++ analysis/HackathonViz.py
@@ -0,0 +1,113 @@
+
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 11 10:48:52 2018
+
+@author: moss
+"""
+
+import random
+import pandas as pd
+from bokeh.io import  curdoc
+from bokeh.palettes import brewer
+from bokeh.models import CustomJS, ColumnDataSource, Panel, HoverTool, CategoricalColorMapper
+from bokeh.plotting import figure,output_file, show
+from bokeh.layouts import row, WidgetBox
+from bokeh.models.widgets import CheckboxGroup, Tabs
+
+
+
+#testData = []
+#x=[]
+#cat=[]
+#for _ in range(100):
+#    testData.append(random.randint(0,100))
+#    x.append(_)
+#    cat.append(random.randint(1,4))
+#    
+#data = pd.DataFrame(data={'x':x,'y':testData,'cat':cat})
+#dataCats = list(str(data['cat'].unique()))
+
+data = pd.read_table('tcga.umap.15.tsv',header=0)
+dataCats = list(data['tissue'].unique())
+source=ColumnDataSource(data=data)
+
+def updateSelection(attr, old, new):
+    newSelection = [selection.labels[i] for i in selection.active]
+    new_source = old[newSelection]
+    source.data.update(new_source)
+    
+def makePlot(source):
+    tools=["box_select","hover","reset"]
+    p = figure(plot_width = 1000, plot_height = 1000,tools=tools)
+    p.circle(x='comp 0',y='comp 1', size = 0.5, hover_color ="black",source=source, color='tissue')
+    hover=HoverTool(tooltips=[('Tissue','@tissue')])
+    p.add_tools(hover)
+    
+    return(p)
+
+
+
+callback=CustomJS(args=dict(source=source), code="""
+			var data=source.data;
+			var tissue=data['tissue'];
+
+			var pc1=data['comp 0'];
+			var pc2=data['comp 1'];
+
+			var data1=source.data;
+			var f=cb_obj.value;
+
+			var x=[]
+			var y=[];
+
+			for(var i=0;i<pc1.length; i++){
+				if(tissue[i]==f){
+					x.push(pc1[i]);
+					y.push(pc2[i]);
+				}
+			}
+			data1['comp 0']=x;
+			data1['comp 1']=y;
+
+			source.change.emit();
+""")
+
+p = makePlot(source)
+selection = CheckboxGroup(labels=dataCats, active = [0,1])
+selection.js_on_change('active',callback)
+controls=WidgetBox(selection)
+layout = row(controls, p)
+tab = Panel(child=layout, title='test')
+tabs=Tabs(tabs=[tab])
+
+#curdoc().add_root(tabs)
+show(tabs)
+
+
+
+
+
+
+#cancer_selection = CheckboxGroup(labels=list(___), active = [0,1])
+#
+#show(p)
+#show(widgetbox(cancer_selection))
+#
+#
+#def makePlot(data):
+#    p = figure(plot_width = 600, plot_height = 600)
+#    p.circle(data, size = 1)
+#    
+#    hover = HoverTool(tooltips=[('Cancer','@___'),
+#                                (')])
+#    
+#
+#def updatePlot(attr, old, new):
+#    cancers_to_plot = [cancer_selection.labels[i] 
+#        for i in cancer_selection.active]
+#    new_src = data[cancers_to_plot]
+#    src.data.update(new_src.data)
+#    
+#cancer_selection.on_change('active',updatePlot)
+
--- bin/decomposition.py
+++ bin/decomposition.py
@@ -8,60 +8,148 @@ from os.path import join
 import pandas as pd
 import umap
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 import seaborn as sns; sns.set()
+import numpy as np
+from mpl_toolkits import mplot3d
+from matplotlib.colors import ListedColormap
 
 def parse_args():
     from argparse import ArgumentParser, FileType
     parser = ArgumentParser(description='Decompose it!')
     parser.add_argument(
-        '--table',
-        type=FileType('r'),
+        '--expression',
         required=True,
         help='Column names and types in TSV format'
     )
     parser.add_argument(
-        '--out_file',
-        type=FileType('w'),
+        '--project',
         required=True,
-        help='TSV format'
+        help='File with tissue assignments'
+    )
+    parser.add_argument(
+        '--tissue_map',
+        type=FileType('r'),
+        required=True,
+        help='File with tissue assignments'
     )
     parser.add_argument(
         '--image_prefix',
         required=True,
         help='Image file prefix'
     )
+    parser.add_argument(
+        '--threeD',
+        action='store_true',
+        default=False,
+        help='Whether to plot 3 components'
+    )
+
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument(
+        '--umap_nn',
+        type=int,
+        help='Number of nearest neighbors for umap'
+    )
+    group.add_argument(
+        '--pca',
+        help='Which PCA type to use'
+    )
+
     return parser.parse_args()
 
-def plot_projection(projected, target, img_prefix):
-    plt.scatter(projected[:, 0], projected[:, 1],
-            c=target, edgecolor='none', alpha=0.5,
-            cmap=plt.cm.get_cmap('nipy_spectral', 10))
-    plt.xlabel('component 1')
-    plt.ylabel('component 2')
-    plt.colorbar();
-    plt.savefig(img_prefix+'.png')
+def plot_3d(projected, img_prefix):
+    fig = plt.figure()
+    fig.set_size_inches(10,6)
+    ax = fig.add_subplot(111, projection='3d')
+    labels = list(set(projected['tissue']))
+    colors = sns.color_palette("husl", len(labels))
+    color_dict = dict(zip(labels, colors))
+    my_cmap = ListedColormap(colors)
+    color_column = [ color_dict[label] for label in projected['tissue'] ]
+    ax1 = ax.scatter(   
+            projected['comp 0'],
+            projected['comp 1'],
+            projected['comp 2'],
+            c = color_column,
+            cmap = my_cmap
+    )       
+    #cbar = plt.colorbar(ax1)
+    cax, _ = mpl.colorbar.make_axes(ax)
+    cbar = mpl.colorbar.ColorbarBase(cax, cmap=my_cmap )
+    cbar.set_ticks(list(range(len(labels))))
+    cbar.set_ticklabels(labels)
+    ax.set_zlabel('component 3')
+
+def plot_projection(projected, img_prefix, threeD):
+    if threeD:
+        plot_3d(projected, img_prefix)
+    else:
+        fig, ax = plt.subplots(figsize=(10,6))
+        sns.scatterplot(ax=ax, data = projected, x = 'comp 0', y = 'comp 1', hue='tissue')
+    ax.set_xlabel('component 1')
+    ax.set_ylabel('component 2')
+    plt.savefig(img_prefix+'.png', dpi=300)
     plt.clf()
 
-def pca_stuff(data, target, img_prefix):
-    pca = sk.decomposition.PCA(2)
+def plot_explained_variance(explained_variance, img_prefix):
+    plt.scatter(range(len(explained_variance)), explained_variance, edgecolor='none', alpha=0.5)
+    plt.xlabel('variance')
+    plt.ylabel('component')
+    plt.savefig(img_prefix+'.explained_var.png')
+    plt.clf()
+
+def pca_stuff(data, img_prefix, pca_name, threeD):
+    decomps = {
+        'PCA':sk.decomposition.PCA(),
+        'KernelPCA':sk.decomposition.KernelPCA(),
+        'SparsePCA':sk.decomposition.SparsePCA()
+    }
+    pca = decomps[pca_name]
     projected = pca.fit_transform(data)
-    plot_projection(projected, target, img_prefix+'.pca')
+    columns=['comp '+str(i) for i in range(projected.shape[1])]
+    df = pd.DataFrame(data=projected, columns=columns)
+    df['tissue'] = data.index.values
+    pca_name = img_prefix+'.'+pca_name
+    df.to_csv(pca_name+'.tsv', sep='\t')
+    plot_projection(df, pca_name, threeD)
+    plot_explained_variance(pca.explained_variance_ratio_, pca_name)
  
-def umap_stuff(data, target, img_prefix):
-    embedding = umap.UMAP(n_neighbors=5, min_dist=0.3, metric='correlation')
+def umap_stuff(data, img_prefix, nn, threeD):
+    ncomps = 3 if threeD else 2
+    embedding = umap.UMAP(n_neighbors=nn, min_dist=0.0, metric='correlation', n_components=ncomps)
     projected = embedding.fit_transform(data)
-    plot_projection(projected, target, img_prefix+'.umap')
+    columns=['comp '+str(i) for i in range(projected.shape[1])]
+    df = pd.DataFrame(data=projected, columns=columns)
+    df['tissue'] = data.index.values
+    outname='.'.join([img_prefix,'umap',str(nn)])
+    df.to_csv(outname+'.tsv', sep='\t')
+    plot_projection(df, outname, threeD)
+
+def parse_projects(project_fh):
+    data = pd.read_table( project_fh, index_col=0 )
+    return dict(zip(data['Sample ID'], data['Project ID'])) 
+
+def parse_tissue_map(tissue_fh):
+    tissue_map = {}
+    for t in tissue_fh:
+        k,sep,v = t.strip().partition('\t')
+        tissue_map[k]=v
+    return tissue_map
 
 def main():
     args = parse_args()
-    #data = pd.DataFrame( args.in_file, sep='\t', header=0 )
-    from sklearn.datasets import load_digits
-    digits = load_digits()
-    data = digits.data
-    target = digits.target
-
-    pca_stuff(data, target, args.image_prefix) 
-    umap_stuff(data, target, args.image_prefix)
+    data = pd.read_table( args.expression, index_col=0 ).transpose()
+    data.apply(np.log1p)
+    projects = parse_projects(args.project)
+    tissues = parse_tissue_map(args.tissue_map)
+    data.index = [projects[i] for i in data.index]
+    data.index = [tissues[i] for i in data.index]
+    #data = data.drop(data.columns[0],axis=1)
+    if args.pca:
+        pca_stuff(data, args.image_prefix, args.pca, args.threeD) 
+    else:
+        umap_stuff(data, args.image_prefix, args.umap_nn, args.threeD)
 
 if __name__ == '__main__':
     main()
